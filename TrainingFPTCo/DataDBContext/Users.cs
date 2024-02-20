cee344f219b1d9f93aa2eee507a3b2714e22fc9e
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2024-02-20 18:32:59 +0700

    update code

---
+++ TrainingFPTCo/DataDBContext/Users.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainingFPTCo.DataDBContext
+{
+    public class Users
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [ForeignKey("RolesId"), Required]
+        public required Roles Roles { get; set; }
+
+        [Column("Username", TypeName="Varchar(100)"), Required]
+        public required string UserName { get; set; }
+
+        [Column("Password", TypeName = "Varchar(100)"), Required]
+        public required string Password { get; set; }
+
+        [Column("Extracode", TypeName = "Varchar(100)"), Required]
+        public required string Extracode { get; set; }
+
+        [Column("Email", TypeName = "Varchar(100)"), Required]
+        public required string Email { get; set; }
+
+        [Column("Phone", TypeName = "Varchar(20)"), Required]
+        public required string Phone { get; set; }
+    }
+}
---
+++ TrainingFPTCo/Queries/LoginQueries.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using TrainingFPTCo.Models;
+
+namespace TrainingFPTCo.Queries
+{
+    public class LoginQueries
+    {
+        // chua cac logic sql xu ly voi database
+        public LoginViewModel CheckLoginUser(string? username, string? password)
+        {
+            LoginViewModel dataUser = new LoginViewModel();
+            using (SqlConnection conn = DatabaseConnection.GetSqlConnection())
+            {
+                string querySql = "SELECT * FROM users WHERE username = @username AND password = @password";
+                // @username va @password : tham so truyen vao cau lenh sql voi gia tri dc nhan tu 2 bien string username va string password
+
+                // tao 1 doi tuong command de thuc thi cau lenh sql
+                SqlCommand cmd = new SqlCommand(querySql, conn);
+                // xu ly truyen gia tri vao cho tham so trong sql
+                cmd.Parameters.AddWithValue("@username", username ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@password", password ?? (object)DBNull.Value);
+                // mo ket noi toi database
+                conn.Open();
+                // thuc thi lenh sql
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // do du lieu tu table trong database vao model minh da dinh nghia
+                        dataUser.Id = reader["Id"].ToString();
+                        dataUser.UserName = reader["Username"].ToString();
+                        dataUser.Email = reader["Email"].ToString();
+                        dataUser.RolesId = reader["RolesId"].ToString();
+                        dataUser.Phone = reader["Phone"].ToString();
+                        dataUser.ExtraCode = reader["ExtraCode"].ToString();
+                        dataUser.FullName = reader["FullName"].ToString();
+                    }
+                    // ngat ket noi toi database
+                    conn.Close();
+                }
+            }
+            return dataUser;
+        }
+    }
+}
